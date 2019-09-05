13ca54ad5b2a5ebe4b898f6a43c2c05217967a17
Mick Letofsky <mick.tosk@gmail.com>
2019-09-05 13:13:23 -0600

    Initial commit

---
+++ src/ContosoUniversity.Web/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Web.Data;
+using ContosoUniversity.Web.Models;
+
+namespace ContosoUniversity.Web.Pages.Courses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContosoUniversity.Web.Data.SchoolContext _context;
+
+        public DetailsModel(ContosoUniversity.Web.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Courses
+                .AsNoTracking()
+                .Include(c => c.Department)
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ src/ContosoUniversity.Web/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Web.Data;
+using ContosoUniversity.Web.Models;
+using ContosoUniversity.Web.Models.SchoolViewModels;
+
+namespace ContosoUniversity.Web.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Web.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Web.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        //public IList<CourseViewModel> CourseVM { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
